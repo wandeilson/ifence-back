4f5a0e52aba0c21b914f3db74c85ddd8c4470557
Jordielson <jordielsonemanuel@gmail.com>
2022-10-25 11:35:26 -0300

    removal of unnecessary comments

--- src/main/java/br/edu/ifpb/dac/groupd/business/service/firebase/PushNotificationService.java
+++ src/main/java/br/edu/ifpb/dac/groupd/business/service/firebase/PushNotificationService.java
@@ -15,14 +15,6 @@ public class PushNotificationService {
     @Autowired
     private FCMService fcmService;
 
-    // public void sendPushNotification(PushNotificationRequest request) {
-    //     try {
-    //         fcmService.sendMessage(getSamplePayloadData(), request);
-    //     } catch (Exception e) {
-    //         logger.error(e.getMessage());
-    //     }
-    // }
-
     public void sendPushNotificationWithoutData(PushNotificationRequest request) {
         try {
             fcmService.sendMessageWithoutData(request);
@@ -38,12 +30,4 @@ public class PushNotificationService {
             logger.error(e.getMessage());
         }
     }
-
-//     private Map<String, String> getSamplePayloadData() {
-//         Map<String, String> pushData = new HashMap<>();
-//         pushData.put("messageId", "msgid");
-//         pushData.put("text", "txt");
-//         pushData.put("user", "pankaj singh");
-//         return pushData;
-//     }
 }
